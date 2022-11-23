d4cf6d5c6864f21da966f7ab88daf04d68f28ffd
mridwann7 <mridwantamvan07@gmail.com>
2022-11-23 22:55:45 +0700

    feat: initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
---
+++ pages/detail.js
@@ -0,0 +1,43 @@
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Meta from "../components/Meta";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Detail() {
+  const router = useRouter();
+  const [title, setTitle] = useState("");
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if (!router.query.data) return;
+    const _data = JSON.parse(router.query.data).data;
+    setTitle(`: ${_data.name}`);
+    setData(_data);
+  }, []);
+
+  return (
+    <div>
+      <Meta title={`Health News${title}`} />
+
+      <Header />
+
+      <section className="text-gray-600 body-font overflow-hidden">
+        <div className="container px-5 py-10 mx-auto">
+          <div className="lg:w-4/5 mx-auto flex flex-wrap">
+            <Image alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src={data ? data.image.src : ""} width="400" height="400" />
+            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+              <h2 className="text-sm title-font text-gray-500 mb-2 tracking-widest">{data ? data.date : ""}</h2>
+              <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">{data ? data.title : ""}</h1>
+              <p className="leading-relaxed">{data ? data.description : ""}</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <Footer />
+    </div>
+  );
+}
